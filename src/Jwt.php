c26a9878aec129822bf9b68637631cca6bb1b688
oghenekparobo <classicsteph@gmail.com>
2024-03-19 03:36:21 +0100

    jwt update

--- src/Jwt.php
+++ src/Jwt.php
@@ -29,37 +29,35 @@ class Jwt
    
     public function decode(string $token): array
     {
-        if (
-            preg_match(
-                "/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/",
-                $token,
-                $matches
-            ) !== 1
-        ) {
-
+        if (preg_match("/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/",
+                   $token,
+                   $matches) !== 1) {
+                       
             throw new InvalidArgumentException("invalid token format");
         }
-
-        $signature = hash_hmac(
-            "sha256",
-            $matches["header"] . "." . $matches["payload"],
-            $this->key,
-            true
-        );
-
-        $signature_from_token = $this->base64URLDecode($matches["signature"]);
-
-        if (!hash_equals($signature, $signature_from_token)) {
-
-            // throw new Exception("signature doesn't match");
+        
+        $signature = hash_hmac("sha256",
+                               $matches["header"] . "." . $matches["payload"],
+                               $this->key,
+                               true);   
+                               
+        $signature_from_token = $this->base64urlDecode($matches["signature"]);
+        
+        if ( ! hash_equals($signature, $signature_from_token)) {
+            
             throw new InvalidSignatureException;
         }
-
-        $payload = json_decode($this->base64URLDecode($matches["payload"]), true);
-
+        
+        $payload = json_decode($this->base64urlDecode($matches["payload"]), true);
+        
+        if ($payload["exp"] < time()) {
+            
+            throw new TokenExpiredException;
+        }
+        
         return $payload;
     }
-
+    
   
     private function base64URLEncode(string $text): string
     {
