5adc27ad9b631b1612de6fe7283096311630779f
oghenekparobo <classicsteph@gmail.com>
2024-03-19 03:56:02 +0100

    finished

--- api/refresh.php
+++ api/refresh.php
@@ -30,3 +30,34 @@ try {
     echo json_encode(["message" => "invalid token"]);
     exit;
 }
+
+$user_id = $payload["sub"];
+
+
+$refresh_token_gateway = new RefreshTokenGateway($database, $_ENV["SECRET_KEY"]);
+
+$refresh_token = $refresh_token_gateway->getByToken($data["token"]);
+
+if ($refresh_token === false) {
+    
+    http_response_code(400);
+    echo json_encode(["message" => "invalid token (not on whitelist)"]);
+    exit;
+}
+                         
+$user_gateway = new UserGateway($database);
+
+$user = $user_gateway->getByID($user_id);
+
+if ($user === false) {
+    
+    http_response_code(401);
+    echo json_encode(["message" => "invalid authentication"]);
+    exit;
+}
+
+require __DIR__ . "/tokens.php";
+
+$refresh_token_gateway->delete($data["token"]);
+
+$refresh_token_gateway->create($refresh_token, $refresh_token_expiry);
