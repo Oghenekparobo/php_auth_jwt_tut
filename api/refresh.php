c6769400f1615032e63e56b817944c0bee537165
oghenekparobo <classicsteph@gmail.com>
2024-03-19 02:22:38 +0100

    refresh token

---
+++ api/refresh.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . "/bootstrap.php";
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    
+    http_response_code(405);
+    header("Allow: POST");
+    exit;
+}
+
+$data = (array) json_decode(file_get_contents("php://input"), true);
+
+if ( ! array_key_exists("token", $data)) {
+
+    http_response_code(400);
+    echo json_encode(["message" => "missing token"]);
+    exit;
+}
+
+$codec = new Jwt($_ENV["SECRET_KEY"]);
